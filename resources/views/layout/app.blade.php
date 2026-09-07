3d778f123dc417cc4e1db01cd5b21be313f79f13
forfamily <forfamiliamanagement@gmail.com>
2026-09-07 14:43:06 +0700

    update gtm dan ga4 tag

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,13 +13,21 @@
         'resources/css/app.css',
         'resources/js/app.js'
     ])
-
+     {{-- =====================================================
+         GOOGLE TAG MANAGER
+    ====================================================== --}}
+    
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NMWV8DFD');</script>
 
     {{-- =====================================================
          GOOGLE ANALYTICS 4
     ====================================================== --}}
 
-    <script async
+   <script async
         src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX">
     </script>
 
@@ -206,5 +214,9 @@
         </main>
         {{-- footer --}}
         @include('components.footer')
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMWV8DFD"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
     </body>
 </html>
